16502a7b56fb844eef613cc310ce244a2f0d4ca5
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 10:38:38 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Snack_Shack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
