631bc78a9ef82ac0bd2fbd9f715255352d2d646a
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 21:26:01 +0000

    Added to Food and Drink Class. Uncompleted.

--- Models/Drink.cs
+++ Models/Drink.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace Snack_Shack.Models
 {
     public class Drink
     {
-      
+        /// <summary>
+        /// The Drink's Category type.
+        /// </summary>
+        [Required]
+        public DrinkCategory DrinkCategory { get; set; }
 
+        public bool ContainsAlcohol { get; set; }
     }
     // Definitely could make use of Enums
+    // Contains Alcohol here? Or seperate Classes for alcohol and non alcoholic drinks
 }
--- Models/Food.cs
+++ Models/Food.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace Snack_Shack.Models
 {
     public class Food
     {
+        /// <summary>
+        /// The Drink's Category type.
+        /// </summary>
+        [Required]
+        public FoodCategory FoodCategory { get; set; }
 
     }
     // Definitely could make use of Enums
     // Could make an Enum for level of spice?!
+    // Seperate categories for "Meat", "Fish", "Veggie" etc.???
 }
