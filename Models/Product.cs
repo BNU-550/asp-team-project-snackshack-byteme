ee69741519c2f5f4f7752bfa650b66599531d2ed
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 12:09:02 +0000

    Model Annotations for Product

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,25 @@ namespace Snack_Shack.Models
 {
     public class Product
     {
+        public int ProductID { get; set; }
+
+        // Make it product name?
+        [DisplayName("Name"), Required, StringLength(20)]
+        public string Name { get; set; }
+
+        [DisplayName("More details"), Required, StringLength(100)]
+        public string Description { get; set; }
+
+        // Do we need a display name if the same?
+        [DisplayName("Price"), Required]
+        public decimal Price { get; set; }
+
+        [DisplayName("Available"), Required]
+        // Needs to be nullable?
+        public bool IsActive { get; set; }
+
+        // Kayley and I don't think it's required
+        [DisplayName("Image"), Required, StringLength(150)]
+        public string ImageURL { get; set; }
     }
 }
