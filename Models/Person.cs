c0031ac59a587be102085b3d1f31eb63b53a8621
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 11:01:52 +0000

    Annotated Person Model

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,24 @@ namespace Snack_Shack.Models
 {
     public class Person
     {
+        public int PersonID { get; set; }
+
+        [DisplayName("Last Name"), Required, StringLength(20)]
+        public string LastName { get; set; }
+
+        [DisplayName("First Name"), Required, StringLength(20)]
+        public string FirstName { get; set; }
+
+        [DisplayName("Email Address"), Required, StringLength(60)]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Over 18?"), Required]
+        // Needs to be nullable?
+        public bool IsMinor { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
