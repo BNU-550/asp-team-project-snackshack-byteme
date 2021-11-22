8f9b1b51a038f2fffd3b14531040ab9baa109edd
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 11:57:55 +0000

    Model Annnotations for Order Item and Payment

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace Snack_Shack.Models
 {
     public class OrderItem
     {
+        // Do we need a limit?
+        [DisplayName("Item Price (£)"), Required]
+        public decimal SalePrice { get; set; }
+
+        [DisplayName("Item Quantity"), Required]
+        public  int Quantity { get; set; }
     }
 }
--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace Snack_Shack.Models
 {
     public class Payment
     {
+        public string CardNo { get; set; }
+
+        // Range too small? Use String instead?
+        [DisplayName("Cardholder Name"), Required, Range(0,60)]
+        public int CardHolderName { get; set; }
+
+        // Range too small? Use String instead?
+        [DisplayName("Expiration Date"), Required, Range(0, 10)]
+        public int ExpiryDate { get; set; }
+
+        // Range too small? Use String instead? Underscore ok?
+        [DisplayName("CVC"), Required, Range(0, 10)]
+        public int CVC_Number { get; set; }
+
     }
 }
