c38b94a29bda77cdaf98da6a0845463a21061ecf
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-11-22 18:16:57 +0000

    Added inheritence and comments to Staff Class

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -1,23 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Snack_Shack.Models
 {
-    public class Staff
+    /// <summary>
+    /// Represents staff members in the database. Staff inherit their
+    /// details such as name, email address, phone number from the
+    /// Person class. The Person primary key is linked to this class.
+    /// 
+    /// The Staff class has their StaffID as their primary key as
+    /// to differeniate which staff member served which order.
+    /// 
+    /// This class also includes the staff members job title and 
+    /// their salary.
+    /// 
+    /// Created Isabelle Thorpe and Kayley Syrett
+    /// Modified by Isabelle Thorpe (22/11/2021)
+    /// </summary>
+    public class Staff : Person
     {
         [Key]
         public int StaffID { get; set; }
-        
-        //Nullable?
+
+        /// <summary>
+        /// The staff member's current job title.
+        /// </summary>
         [DisplayName("Job Title"), StringLength(20)]
         public string JobTitle { get; set; }
         
-        // No DisplayName?
+        /// <summary>
+        /// The staff members current salary.
+        /// </summary>
         public decimal Salary { get; set; }
-
     }
 }
